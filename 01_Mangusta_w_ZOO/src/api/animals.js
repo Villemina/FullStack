1cea65a11409bdc5838bd076c49f0441af582a57
agnieszka <agnieszka.gos.1987@com>
2023-10-17 20:16:29 +0200

    mangusta w zoo / handlebar training

---
+++ 01_Mangusta_w_ZOO/src/api/animals.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import Animals from '../services/animals';
+
+const { Router } = express;
+const router = Router();
+
+
+const animals = new Animals();
+
+router.get('/', async (req, res) => {
+  return res.json({});
+});
+
+router.get('/add', async (req, res) => {
+  return res.json({});
+});
+
+router.get('/remove', async (req, res) => {
+  return res.json({});
+});
+
+router.post('/add', async (req, res) => {
+  return res.json({});
+});
+
+export default router;
